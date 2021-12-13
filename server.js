615d15efae96a339f80ff0147fa2cc827e5193dc
Braulio Mora <brauliocdc59@gmail.com>
2021-12-12 22:07:05 -0800

    updated server.js with comments about the post route logic

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const api = require('./routes/index.js');
 const notes = require('./db/db.json');
 const {writeFile} = require('fs');
 const { v4: uuidv4 } = require('uuid');
@@ -14,8 +13,8 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use('/api', api);
 
+// Middleware for serving static pages and files
 app.use(express.static('public'));
 
 // GET Route for homepage
@@ -36,28 +35,27 @@ app.get('/api/notes', (req, res) => {
 
 //Post Route for saving new notes into db
 app.post('/api/notes', (req, res) => {
-    // 
     console.log(`\n******************************\n${req.method} request received to add a note!\n******************************`);
 
-    // 
+    // Destructures request body for new vars containing note details
     const { title, text} = req.body;
   
-    // 
+    // Checks for truish note content from the request
     if (title && text) {
-      // 
+      // Builds new note post body and generates a unique id for the note
       const newNote = {
         title, 
         text,
         id: uuidv4(),
       };
       
-      //
+      //will push the new note into the db array
       notes.push(newNote);
 
-      // 
+      // stringifies the updated notes array
       const dbNotesString = JSON.stringify(notes);
   
-      // 
+      // Over writes the db.json file with updated notes array
       writeFile(`./db/db.json`, dbNotesString, (err) => 
         err
           ? console.error(err)
@@ -65,15 +63,19 @@ app.post('/api/notes', (req, res) => {
               `\n******************************\nNew note: ${JSON.stringify(newNote)} has been written to the notes JSON file\n******************************`
             )
       );
-  
+      
+      // builds a response body
       const response = {
         status: 'success',
         body: newNote,
       };
   
       console.log(`\n******************************\nserver has responded with body: ${JSON.stringify(response)}\n******************************`);
+      
+      //responds with a status code (created 201) and response body with status and newly generated note
       res.status(201).json(response);
     } else {
+      //otherwise sends a 500 and details about failing to post the new note
       res.status(500).json('Error in posting new note');
     }
   })
